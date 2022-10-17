3cd342a229c5fdf09c80d86259af470f3e310917
albilaga <albilaga.lp@gmail.com>
2022-10-17 15:01:37 +0700

    Add initial db

---
+++ RAS.Bootcamp.Api.Net/Migrations/20221017033336_Initial DB.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RAS.Bootcamp.Api.Net.Migrations
+{
+    public partial class InitialDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Tipe = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pembelies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    IdUser = table.Column<int>(type: "integer", nullable: false),
+                    Alamat = table.Column<string>(type: "text", nullable: false),
+                    NoHp = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pembelies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pembelies_Users_IdUser",
+                        column: x => x.IdUser,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Penjuals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    IdUser = table.Column<int>(type: "integer", nullable: false),
+                    NamaToko = table.Column<string>(type: "text", nullable: false),
+                    Alamat = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Penjuals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Penjuals_Users_IdUser",
+                        column: x => x.IdUser,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Barangs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Kode = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    Nama = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Harga = table.Column<decimal>(type: "numeric", nullable: false),
+                    Stok = table.Column<int>(type: "integer", nullable: false),
+                    IdPenjual = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Barangs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Barangs_Penjuals_IdPenjual",
+                        column: x => x.IdPenjual,
+                        principalTable: "Penjuals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Barangs_IdPenjual",
+                table: "Barangs",
+                column: "IdPenjual");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pembelies_IdUser",
+                table: "Pembelies",
+                column: "IdUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Penjuals_IdUser",
+                table: "Penjuals",
+                column: "IdUser");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Barangs");
+
+            migrationBuilder.DropTable(
+                name: "Pembelies");
+
+            migrationBuilder.DropTable(
+                name: "Penjuals");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
