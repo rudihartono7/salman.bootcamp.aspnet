2ec5a1de7dcbe1bb7315aa2d48f432029bf79a14
Iwa Sugriwa <iwa@radyalabs.com>
2022-10-20 11:37:08 +0700

    delete unused file

--- RAS.Bootcamo.Catalog.Mvc.Net/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace RAS.Bootcamo.Catalog.Mvc.Net.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
