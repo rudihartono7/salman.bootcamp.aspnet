2600e50543f11d7b7f16e8103c4036c544e081e8
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2022-10-25 15:46:49 +0700

    Add order in catalog

--- RAS.Bootcamp.Catalog.Mvc.Net/Controllers/OrderController.cs
+++ RAS.Bootcamp.Catalog.Mvc.Net/Controllers/OrderController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RAS.Bootcamp.Catalog.Mvc.Net.Datas;
 
 namespace RAS.Bootcamp.Catalog.Mvc.Net.Controllers;
 
 [Authorize]
 public class OrderController : Controller
 {
+    private readonly EMarketDbContext _dbContext;
+
+    public OrderController(EMarketDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
     /// <summary>
     /// Yg ditampilkan adalah
     /// 1. Statusnya adalah waiting for payment (menunggu pembayaran)
@@ -17,17 +27,51 @@ public class OrderController : Controller
     /// <returns></returns>
     public IActionResult Index()
     {
-        return View();
+        var userId = User.Claims.First(e => e.Type == ClaimTypes.NameIdentifier).Value;
+        var id = int.Parse(userId);
+
+        var listOrder = _dbContext.Transaksis
+            .Where(e => e.IdUser == id)
+            .ToList();
+
+        return View(listOrder);
     }
 
     public IActionResult Detail(int id)
     {
-        return View();
+        var transaksi = _dbContext.Transaksis.FirstOrDefault(e => e.Id == id);
+        if (transaksi is null)
+            return RedirectToAction("Index");
+
+        var userId = User.Claims.First(e => e.Type == ClaimTypes.NameIdentifier).Value;
+        var userIdInInt = int.Parse(userId);
+
+        if (transaksi.IdUser != userIdInInt)
+            return RedirectToAction("Index");
+
+        return View(transaksi);
     }
 
     [HttpPost]
-    public IActionResult OrderPayment()
+    public IActionResult OrderPayment([FromForm] int id)
     {
+        var userId = User.Claims.First(e => e.Type == ClaimTypes.NameIdentifier).Value;
+        var userIdInInt = int.Parse(userId);
+
+        var transaksi = _dbContext.Transaksis.FirstOrDefault(e => e.Id == id);
+        if (transaksi is null)
+            return RedirectToAction("Index");
+
+        if (transaksi.IdUser != userIdInInt)
+            return RedirectToAction("Index");
+
+        if (transaksi.StatusTransaksi != "WAITINGFORPAYMENT")
+            return RedirectToAction("Index");
+
+        transaksi.StatusTransaksi = "PAYED";
+        
+        _dbContext.SaveChanges();
+
         return RedirectToAction("Index");
     }
 
